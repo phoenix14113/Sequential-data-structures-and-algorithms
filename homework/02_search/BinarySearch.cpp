b208b9e6fc217192e433ee289a6077754542d3d3
Adam Taylor <adam.taylor@wallawalla.edu>
2021-04-06 22:04:37 +0000

    created both search algorithms

--- homework/02_search/BinarySearch.cpp
+++ homework/02_search/BinarySearch.cpp
@@ -7,8 +7,37 @@
  * Course:      CPTR 242
  * Date:        ?
  */
-#include "ItemType.h"
 #include "BinarySearch.h"
+#include "ItemType.h"
 
 // TODO implement search
 
+void BinarySearch::Search(std::vector<ItemType> list, ItemType key,
+                          int &location, int &comparisons) {
+  comparisons = 0;
+  location = -2;
+  int listMax = MAX_ITEMS - 1;
+  int listMin = 0;
+  int listMid = 0;
+  while (listMax >= listMin) {
+    comparisons++;
+    listMid = (listMax + listMin) / 2;
+    switch (key.ComparedTo(list.at(listMid))) {
+    case LESS:
+      listMax = listMid - 1;
+      break;
+    case GREATER:
+      listMin = listMid + 1;
+      break;
+    case EQUAL:
+      location = listMid;
+      break;
+    }
+    if(location != -2){
+        break;
+    }
+  }
+  if(location == -2){
+      location = -1;
+  }
+}
--- homework/02_search/LinearSearch.cpp
+++ homework/02_search/LinearSearch.cpp
@@ -7,7 +7,28 @@
  * Course:      CPTR 242
  * Date:        ?
  */
-#include "ItemType.h"
 #include "LinearSearch.h"
+#include "ItemType.h"
 
 // TODO implement search
+void LinearSearch::Search(std::vector<ItemType> list, ItemType key,
+                          int &location, int &comparisons) {
+  comparisons = 0;
+  location = -2;
+  for (int i = 0; i < list.max_size(); i++) {
+    comparisons++;
+    switch (key.ComparedTo(list.at(i))) {
+    case LESS:
+      location = -1;
+      break;
+    case GREATER:
+      break;
+    case EQUAL:
+      location = i;
+      break;
+    }
+    if (location != -2) {
+      break;
+    }
+  }
+}
