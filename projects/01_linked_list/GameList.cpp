a6f1cfac94aa6fbe945de41af769ed5abb55e718
Adam Taylor <adam.taylor@wallawalla.edu>
2021-04-23 18:35:13 +0000

    customized visualization

--- projects/01_linked_list/GameList.cpp
+++ projects/01_linked_list/GameList.cpp
@@ -62,40 +62,88 @@ SLelement<string> *GameList::GetHead() { return listData; }
 
 void GameList::PutGame(std::string title, std::string gameDetails,
                        double rating) {
-  // TODO Add code here.
+  // create new node.
   SLelement<std::string> *newGame = new SLelement<std::string>;
   newGame->setLabel(title + "\n" + gameDetails);
   newGame->setValue(title);
 
+  // visualization customization
+  // size of node
+  float size = 0;
+  if (rating < 1) {
+    size = 10.0f;
+  } else if (rating < 2) {
+    size = 11.0f;
+  } else if (rating < 3) {
+    size = 12.5f;
+  } else if (rating < 4) {
+    size = 15.0f;
+  } else if (rating < 5) {
+    size = 18.0f;
+  } else if (rating < 6) {
+    size = 21.5f;
+  } else if (rating < 7) {
+    size = 25.5f;
+  } else if (rating < 8) {
+    size = 31.0f;
+  } else if (rating < 9) {
+    size = 38.0f;
+  } else if (rating < 10) {
+    size = 45.5f;
+  } else {
+    size = 60.0f;
+  }
+  newGame->getVisualizer()->setSize(size);
+
+  // color of node
+  std::string color;
+  if (rating <= 2) {
+    color = "darkred";
+  } else if (rating <= 4) {
+    color = "red";
+  } else if (rating <= 6) {
+    color = "orange";
+  } else if (rating <= 8) {
+    color = "yellow";
+  } else if (rating < 10) {
+    color = "green";
+  } else {
+    color = "blue";
+  }
+  newGame->getVisualizer()->setColor(color);
+
+  // adding elements to the list
   if (length == 0) {
+    // empty list
     listData = newGame;
     length++;
   } else {
+    // not empty list
     ResetList();
     SLelement<std::string> *previous = new SLelement<std::string>;
     for (int i = 1; i <= length; i++) {
-      // std::cout << "hi" << std::endl;
-
       if (currentPos->getValue() > title &&
           currentPos->getValue() == listData->getValue()) {
-
+        // first node case
+        // insert the new node before the first node greater than it
         newGame->setNext(currentPos);
         previous->setNext(newGame);
         listData = newGame;
         length++;
         break;
       } else if (currentPos->getValue() > title) {
-        // std::cout << "hi" << std::endl;
+        // insert the new node before the first node greater than it
         newGame->setNext(currentPos);
         previous->setNext(newGame);
         length++;
         break;
       } else if (i == length) {
+        // final element case
         currentPos->setNext(newGame);
         length++;
-
         break;
       } else {
+        // compare next node
         previous = currentPos;
         currentPos = currentPos->getNext();
       }
@@ -106,42 +154,48 @@ void GameList::PutGame(std::string title, std::string gameDetails,
 
 void GameList::GetGame(std::string title, std::string &gameDetails,
                        bool &found) {
-  // TODO Add code here.
+  // determine if title is in the list
   found = false;
-  SLelement<std::string> *previous = nullptr;
   for (int i = 0; i < length; i++) {
     if (currentPos->getValue() == title) {
+      // if it is the current node
       gameDetails = currentPos->getValue();
       found = true;
       ResetList();
       return;
     } else if (currentPos->getValue() > title) {
+      // if the current node is greater it won't be in the list
       ResetList();
       return;
     }
-    previous = currentPos;
+    // if it is neither equal to or greater than compare the next element
     currentPos = currentPos->getNext();
   }
 }
 
 void GameList::DeleteGame(std::string title) {
-  // TODO Add code here.
+  // find and delete a node
   SLelement<std::string> *previous = nullptr;
   SLelement<std::string> *current = listData;
+  // search the list for the element
   for (int i = 0; i < length; i++) {
     if (current->getValue() == title) {
+      // if it is in the list delete it
       if (current = listData) {
+        // first node case
         listData = current->getNext();
         delete current;
         length--;
         break;
       } else {
+        // other node case
         previous->setNext(current->getNext());
         delete current;
         length--;
         break;
       }
     } else if (current->getValue() > title) {
+      // if it is ever greater
       break;
     }
     previous = current;
