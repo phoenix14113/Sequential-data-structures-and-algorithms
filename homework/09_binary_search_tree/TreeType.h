789bca879c3a0a7ed5df58c2745f82952885a400
Adam Taylor <adam.taylor@wallawalla.edu>
2021-05-28 17:57:45 +0000

    revert

--- homework/09_binary_search_tree/TreeType.h
+++ homework/09_binary_search_tree/TreeType.h
@@ -48,11 +48,11 @@ public:
 
   // Post: Number of nodes with values greater than input value returned.
   int CountGreaterThan(ItemType value);
+
+private:
   int CountLeavesRecusive(TreeNode *tree);
   int CountSingleChildrenRecusive(TreeNode *tree);
   void FindItemGreaterThan(TreeNode *tree, ItemType value, int &total);
-
-private:
   TreeNode *root;
   QueType preQue;
   QueType inQue;
